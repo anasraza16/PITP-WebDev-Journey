8f5b630b5fcac45df424ec79c6e4a847d18fb827
Ahmed Raza <razaplus360@gmail.com>
2026-03-22 08:12:29 +0500

    added class 1 of JS

---
+++ JavaScript/JS Class-01/app.js
@@ -0,0 +1,105 @@
+// // ARITHEMATIC OPERATORS +,-,*,/,%
+// // Arithematic Operation
+
+// // subtraction
+// console.log(5-3);
+// // addition
+// console.log(5+3);
+// // multiplication
+// console.log(5*3);
+// // division
+// console.log(5/3);
+// // modulas
+// console.log(5%3);
+
+
+// // VARIABLES
+
+// a = 20;
+// b = 30;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// // VARIABLES NAMES
+// // var, let, const
+
+// let num = 5;
+// num = 11;
+// console.log(num);
+
+// var number = 3;
+// var number = 2;
+// console.log(number);
+
+
+// // VARIABLE PROCESS
+
+// let a                           // Declaration
+// a = 20;                         // Defination | Initialization
+
+
+// // PRIMITIVE DATA TYPES         // Only for single value
+
+// // number
+// let number = 5;                 // binary and decimal both
+
+// // string
+// let string = "String"           // numbers and alphabet both
+
+// // boolean
+// let boolean = true;             // True or Fales only
+
+// // undefined
+// let undefined                   // did not defined the value
+
+// // null 
+// let universityName = null;      // Did  not exist
+
+// // symbol
+// let symbol = Symbol("NED");     // Unique Indentifier
+
+
+// COMPARISON OPERATORS
+
+// ==                               // only value check in numbers
+// ====                             // Can check data types like number & string
+// !==                              // not equal to
+// >                                // greater than
+// <                                // less than
+// >=                               // greater than n euqals to
+// <=                               // less than n euqals to
+
+// console.log(5 == 5);
+// console.log("5" === 5);
+// console.log(3!=3);
+// console.log(10>3);
+// console.log(10<31);
+// console.log(1 >= 3);
+// console.log(40 <= 10);
+
+
+// LOGICAL OPERATORS
+
+// 1. AND &&
+// console.log(true && true);
+
+// 2. OR |
+// console.log(false || true);
+
+// 3. NOT
+// console.log(!false);
+
+
+
+
+
+
+
+
+
+
+
+
