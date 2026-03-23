80aa8a55ef913b8b752b3d5defa79ed37711532a
Ahmed Raza <razaplus360@gmail.com>
2026-03-23 08:47:54 +0500

    js class 2 added

---
+++ JavaScript/JS Class-02/app.js
@@ -0,0 +1,90 @@
+// ARITHEMATIC OPERATOR
+// +, -, /, *, %
+
+// LOGICAL OPERATOR
+// ||, &&
+
+// COMPARISON OPERATOR
+//    
+
+// NOT OPERATOR
+// !
+
+// ASSIGNMENT OPERATOR
+// =, +, 
+
+// let num = 2;
+// num+7;
+// // num = num + 4;
+// console.log(num);
+
+
+// Increment & Decrement Operator
+
+// let num = 1;
+// console.log(num--);
+// console.log(num);
+
+
+// if/else conditions / statements
+
+// let userAge = prompt("Enter your Age!");
+
+// if (userAge >= 18) {
+//     console.log("You are Qualified for Driving License!");
+    
+// }else {
+//     console.log("Stay at home and play with toys!");
+    
+// }
+
+// let age = prompt("Enter your Age for vote!");
+
+// if (age < 18) {
+//     console.log("You can't Vote!");
+    
+// }else {
+//     console.log("You can Vote!");
+    
+// }
+
+// IF CONDITIONS
+
+// let day  = prompt();
+
+// if (day == "sunday" || day == "saturday") {
+//     console.log("This is Weekend");
+    
+// }else if (day == "friday") {
+//     console.log("Its a Prayer Day");
+    
+// }else {
+//     console.log("Its a Weekday");   
+// }
+
+// let userName = "raza";
+// let password = "123";
+
+// if (userName == "raza" && password == "123") {
+//     console.log("Login");
+    
+// }else {
+//     console.log("You can't Login"); 
+// }
+
+// let userInt = prompt("Enter a single number");
+// alert("Your Entered " + userInt);
+
+// if (userInt < 0) {
+//     console.log("Negative");
+    
+// }else if (userInt > 0 ) {
+//     console.log("Positive");
+    
+// }else {
+//     console.log("Zero");
+// }
+
+
+let userInput = String(prompt("Type a number"));
+console.log(typeof userInput);
