6977ae17662762e26cf0d5216bfc84faba580998
Ahmed Raza <razaplus360@gmail.com>
2026-03-24 23:20:25 +0500

    added 3rd class of js

---
+++ JavaScript/JS Class-03/app.js
@@ -0,0 +1,43 @@
+// let variable = Number(prompt("Enter your age!"));
+// console.log(variable);
+
+// LOOP
+// for loop, while loop, do while loop
+
+// let greet = "Hello World!";
+
+// for (let i = 1; i <= 15; i++) {
+//     console.log(greet);
+//     console.log(i);
+    
+// }
+
+
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum+=i
+// }
+// console.log(sum)
+
+
+// CONDITIONS IN FOR LOOP
+
+for (let i = 0; i <= 5; i++) {
+    if (i==5) {
+        console.log("5");
+        
+    }else {
+        console.log(" not 5");
+        
+    }
+    // console.log(i);
+}
+
+// ARRAYS (put a data for a single category also known as "LIST")
+
+// let students = ["John", "Taha", "Krish"];
+// console.log(students[1]);
+
+// let arr = [1, 2, true, 4, "john", 6, false, 8, ["ali", "raza", "pardeep"]];
+// console.log(arr[8][2]);
+
