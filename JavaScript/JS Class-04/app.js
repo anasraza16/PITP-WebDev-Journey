885bad8b7b0ad83d77fc3e7d050aafc6748d0e9b
Ahmed Raza <razaplus360@gmail.com>
2026-03-25 23:26:42 +0500

    added class 4 of js

---
+++ JavaScript/JS Class-04/app.js
@@ -0,0 +1,63 @@
+// let arr = [1, 2, "john", 55, "adam", [1, 2, 3,], true, "raza", false]
+// console.log(arr[5][1]);
+
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Salam");
+    
+// }
+
+// let name=prompt("enter your name");
+// let gender=prompt("enter your gender");
+
+// if (gender==="male") {
+//     console.log("MR:" + name )
+// }
+// else if(gender==="female") {
+//     console.log("MISS:" + name)
+// }
+// else {
+//     alert(name + " is a Good BOY!");
+// }
+
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+
+
+// let studentdNames = ["raza", "pardeep", "muneeb", "muneer", "ghazanfar", "rana"];
+
+// for (let i = 0; i <= studentdNames-1; i++) {
+//     console.log(studentdNames[i]);
+// }
+
+// let value = [9,2,7,10,5]
+
+// for (let i = 0; i <= value.length-1; i++) {
+//     console.log(value[i]+1);
+// }
+
+let stdData = {
+    stdName: "sarah",
+    course: "Web Dev",
+    rollNo: 123456,
+    gender: "female",
+    marks: 99
+}
+
+let employData = {
+    empName: "Muneeb",
+    designation: "Web Developer",
+    salary: 2500,
+}
+
+let stdObj = [
+    {name: "ali", marks: 78,},
+    {name: "raza", marks: 96,},
+    {name: "muneeb", marks: 69,},
+]
+console.log(stdObj);
+
+
+let users = [
+    {id: 1, name: "user1", password: 34334},
+]
