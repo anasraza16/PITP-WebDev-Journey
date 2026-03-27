b5ad713b84cc2309cbc42298521e019f77b75c78
Ahmed Raza <razaplus360@gmail.com>
2026-03-27 23:49:05 +0500

    added js class 5

---
+++ JavaScript/JS Class-05/app.js
@@ -0,0 +1,56 @@
+// let userName = prompt("Enter your Name!").toLocaleLowerCase
+// let gender = prompt("Enter your Gender!").toLowerCase
+
+// if (userName === "pardeep") {
+//     console.log("pardeep is Good")
+    
+// }else if (gender === "male") {
+//     console.log("MR. " + userName)
+
+// }else if (gender === "female") {
+//     console.log("Miss. " + userName)
+    
+// }else  {
+//     console.log("Invalid");   
+// }
+
+
+// ARRAY METHODS
+
+// 1, push (will add the new element on last)
+// 2, pop  (will remove the last element)
+// 3, shift (will remove the first element on start)
+// 4, unshift (will add the new element on start)
+
+// let students = ["Ali", "Hamza", "Sarah"];
+// students.push("raza");
+// // students.pop();
+// // students.shift();
+// // students.unshift("raza");
+// console.log(students);
+
+
+// WHILE LOOP 
+
+// let i = 100;
+// while (i >= 1) {
+//     console.log(i);()
+//     i--
+// }
+
+// let password = prompt("Enter Password!");
+// let correctPassword = 123;
+
+// while (password != correctPassword) {
+//     password=prompt("Incorrect Password")
+// }
+// alert("Login Successfully!")
+
+
+// DO WHILE LOOP
+
+let i = 51;
+do {
+    console.log("i = " + i);
+    i++
+} while (i <= 50);
