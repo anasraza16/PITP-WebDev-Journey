1959be7d5d2e7edcdfd69e5bcd3d9adf51e07fe0
Ahmed Raza <razaplus360@gmail.com>
2026-04-01 06:17:33 +0500

    added class 7 of js

---
+++ JavaScript/JS Class-07/app.js
@@ -0,0 +1,59 @@
+// let input = prompt("Enter your Name")
+
+// function greet(name) {
+//     console.log("Hello! " + input);
+    
+// }
+// greet()
+
+
+// let heading = document.getElementById("title")
+// console.log(heading);
+
+// // heading.textContent="Hello World!"
+// heading.innerText="Hello Worldly!"
+
+// console.log(heading.innerText);
+
+// document.getElementById("new").style.backgroundColor='green'
+
+// DOM MANIPULATION ON TEXT
+
+let para = document.getElementById("paragraph")
+console.log(para);
+
+// console.log(para.innerText);
+// console.log(para.textContent);
+console.log(para.innerHTML);
+
+
+// DOM MANIPULATION ON STYLING & ATTRIBUTE
+
+// let box = document.getElementById("container")
+
+// box.style.backgroundColor="red"
+// box.style.color="white"
+// box.setAttribute("style", "color: red; background-color: green;") // it will overwrite the previous styling
+// box.classList.add("drum")
+// console.log(box.classList);
+
+
+// EVENTS
+
+// let darkButton = document.getElementById("btn1");
+
+// darkButton.addEventListener("click", function () {
+//     document.body.style.backgroundColor="black"
+//     document.body.style.color="white"
+ 
+// } )
+
+// let lightButton = document.getElementById("btn2");
+
+// lightButton.addEventListener("click", function () {
+//     document.body.style.backgroundColor="white"
+//     document.body.style.color="black"
+ 
+// } )
+
+
